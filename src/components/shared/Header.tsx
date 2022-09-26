f7bc050f18831765a384a5d865bcf83a579ecda1
washimon7 <washing.coen@gmail.com>
2022-09-26 07:21:46 -0500

    Header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,3 +1,18 @@
+import LogoRimac from '../../assets/images/logo-rimac.png';
+import PhoneIcon from '../../assets/icons/ic_phone.svg';
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className='header'>
+      <img className='header__logo' src={LogoRimac} alt='Rimac Seguros' />
+      <div className='header__contact'>
+        <span className='header__has-doubt'>¿Tienes alguna duda?</span>
+        <p className='header__phone'>
+          <img className='header__phone-icon' src={PhoneIcon} alt='Teléfono' />
+          <span className='header__call-us'>Llámanos</span>
+          <span className='header__phone-number'>(01) 411 6001</span>
+        </p>
+      </div>
+    </header>
+  );
 };
